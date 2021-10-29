1c9286868494acef731c17a4356f133805de43a9
Nikita <kotenkov.nikita@mail.ru>
2021-10-29 07:55:15 +0300

    Final commit

--- src/main/java/web/controller/CarsController.java
+++ src/main/java/web/controller/CarsController.java
@@ -4,18 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import web.service.CarService;
 
 @Controller
+@RequestMapping("/cars_controller")
 public class CarsController {
 
-    private final CarService carService;
-
     @Autowired
-    public CarsController(CarService carService) {
-        this.carService = carService;
-    }
+    private CarService carService;
 
     @GetMapping(value = "/cars")
     public String getCars(@RequestParam(value = "count", required = false) Integer count, Model model) {
